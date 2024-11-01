4c27da5c3d3fd6479cdeb10191503073da2b3f03
Woarang <woarang00@gmail.com>
2024-11-01 10:45:25 -0400

    Add files via upload

---
+++ 2024 10 30 - a.py
@@ -0,0 +1,27 @@
+#
+def triplelt(myNum):
+    returnValue = myNum * 3
+    myNum = myNum + 1 # this one have nothing to do in this block 'coz return returnValue (not return myNum)
+    return returnValue
+
+
+myNum = 6
+newNum = triplelt(myNum)
+
+print(newNum)
+
+#
+def doublet(a):
+    a = a * 2
+    return a
+
+a = int(input("Input your number here: "))
+print("a is:", a)
+print("double a is:", doublet(a))
+
+#
+def doublet(b):
+    b = b * 2
+    return b
+b = input("Over here: ")
+print(doublet(b))
